e777e7bdb7e65b1648806ef63d1893595c977db2
PrincipeLibero <128900874+PrincipeLibero@users.noreply.github.com>
2026-04-29 21:10:21 +0200

    Update RomanPrinterTest.java

--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -156,4 +156,18 @@ public class RomanPrinterTest {
 
         assertEquals(expected, result);
     }
+    @Test
+public void shouldPrint20AsAsciiArt() {
+    String expected =
+        "   __   _\n" +
+        "\ \ / /\ \ / /\n" +
+        " \ V /  \ V / \n" +
+        "  > <    > <  \n" +
+        " / . \  / . \ \n" +
+        "// \\// \_\";
+
+    String result = RomanPrinter.print(20);
+
+    assertEquals(expected, result);
+}
 }
